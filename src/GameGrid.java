56ab5910b928b166252b97f34fe2cad8cf603f7a
gayathripuram <48146947+gayathripuram@users.noreply.github.com>
2022-04-30 23:36:36 -0600

    Update GameGrid.java

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -19,7 +19,7 @@ public class GameGrid extends JPanel implements Runnable, MouseListener, Compone
 
     private static final Color black = new Color(0,0,0);
     private boolean gameSimStatus = false;
-    private Dimension gridSize = null;
+    private Dimension SizeOfGrid = null;
     private ArrayList<Point> point = new ArrayList<>(0);
     private static final int CELL_SIZE = 20;
     private HashMap<Point,ArrayList<Point>> map = new HashMap<>();
@@ -45,7 +45,7 @@ public class GameGrid extends JPanel implements Runnable, MouseListener, Compone
     public void drawDot(MouseEvent e){
         int x = e.getPoint().x/CELL_SIZE-1;
         int y = e.getPoint().y/CELL_SIZE-1;
-        if ((x >= 0) && (x < gridSize.width) && (y >= 0) && (y < gridSize.height)) {
+        if ((x >= 0) && (x < SizeOfGrid.width) && (y >= 0) && (y < SizeOfGrid.height)) {
             drawDot(x,y);
         }
     }
@@ -56,10 +56,10 @@ public class GameGrid extends JPanel implements Runnable, MouseListener, Compone
         point.clear();
     }
 
-    public void updateGridSize(){
+    public void updateGrid(){
         ArrayList<Point> removeList = new ArrayList<>(0);
         for (Point current : point) {
-            if ((current.x > gridSize.width-1) || (current.y > gridSize.height-1)) {
+            if ((current.x > SizeOfGrid.width-1) || (current.y > SizeOfGrid.height-1)) {
                 removeList.add(current);
             }
         }
@@ -80,18 +80,18 @@ public class GameGrid extends JPanel implements Runnable, MouseListener, Compone
         }
 
         gr.setColor(Color.white);
-        for (int i=0; i<=gridSize.width; i++){
-            gr.drawLine(((i*CELL_SIZE)+CELL_SIZE), CELL_SIZE, (i*CELL_SIZE)+CELL_SIZE, CELL_SIZE + (CELL_SIZE*gridSize.height));
+        for (int i=0; i<=SizeOfGrid.width; i++){
+            gr.drawLine(((i*CELL_SIZE)+CELL_SIZE), CELL_SIZE, (i*CELL_SIZE)+CELL_SIZE, CELL_SIZE + (CELL_SIZE*SizeOfGrid.height));
         }
-        for (int i=0; i<=gridSize.height; i++){
-            gr.drawLine(CELL_SIZE, ((i*CELL_SIZE)+CELL_SIZE), CELL_SIZE*(gridSize.width+1), ((i*CELL_SIZE)+CELL_SIZE));
+        for (int i=0; i<=SizeOfGrid.height; i++){
+            gr.drawLine(CELL_SIZE, ((i*CELL_SIZE)+CELL_SIZE), CELL_SIZE*(SizeOfGrid.width+1), ((i*CELL_SIZE)+CELL_SIZE));
         }
     }
 
     @Override
     public void componentResized(ComponentEvent e){
-        gridSize = new Dimension(getWidth()/CELL_SIZE-2, (getHeight())/CELL_SIZE-2);
-        updateGridSize();
+    	SizeOfGrid = new Dimension(getWidth()/CELL_SIZE-2, (getHeight())/CELL_SIZE-2);
+        updateGrid();
     }
 
     @Override
@@ -138,7 +138,7 @@ public class GameGrid extends JPanel implements Runnable, MouseListener, Compone
 
     @Override
     public void run(){
-        boolean[][] gameGrid = new boolean[gridSize.width+2][gridSize.height+2];
+        boolean[][] gameGrid = new boolean[SizeOfGrid.width+2][SizeOfGrid.height+2];
         for(Point current : point) {
             gameGrid[current.x+1][current.y+1] = true;
         }
